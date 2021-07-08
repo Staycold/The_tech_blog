fb27eb0ab431cef37062c71eab682cfa0a45fca4
patrick brodie <patrickxbrodie@hotmail.com>
2021-07-07 22:36:41 -0400

    added seed data and login form

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,7 +1,10 @@
 const sequelize = require('../config/connection');
-const { User } = require('../models');
+const { User, Post, Comment} = require('../models');
+
 
 const userData = require('./userData.json');
+const postData = require('./postData.json')
+const commentData = require('./commentdata.json')
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -9,9 +12,16 @@ const seedDatabase = async () => {
   await User.bulkCreate(userData, {
     individualHooks: true,
     returning: true,
+  })
+  await Post.bulkCreate(postData, {
+    individualHooks: true,
+    returning: true,
+  })
+  await Comment.bulkCreate(commentData, {
+    individualHooks: true,
+    returning: true,
   });
-
-
+  
   
 
   process.exit(0);
